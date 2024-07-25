c6ee98c1bb39ff3d7efe7cb5937fb9100fcadff7
rishavkr1123 <rishavitkumar@gmail.com>
2024-07-25 22:33:53 +0530

    Initial commit

---
+++ Lec_1/script.js
@@ -0,0 +1,36 @@
+
+const parent= React.createElement('div',{
+    id:'parent'
+},
+[
+React.createElement('div',{
+    id:'child'
+},
+[React.createElement("h1",{
+    id:'heading'
+}, "Hey How are you"), 
+React.createElement("h2",{
+    id:'heading2'
+}, "Hey How are you") ]
+),
+React.createElement('div',{
+    id:'child'
+},
+[React.createElement("h1",{
+    id:'heading'
+}, "Hey How are you"), 
+React.createElement("h2",{
+    id:'heading2'
+}, "Hey How are you") ]
+)
+]
+)
+console.log(parent)
+
+// const heading = React.createElement("h2",{
+//     id:'heading'
+// }, "Hey How are you") //heading here represents a virtual DOM element <h2>
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
+
+
