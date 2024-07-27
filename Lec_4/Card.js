f85e9a1ab38103532f000946ec80286091c6ed3e
rishavkr1123 <rishavitkumar@gmail.com>
2024-07-28 03:02:45 +0530

    first commit

---
+++ Lec_4/Card.js
@@ -0,0 +1,20 @@
+const Card = ()=>{
+    return(
+        <div className="card-container">
+            <img src=""/>
+            <div>
+                <h3></h3>
+                <p></p>
+                <div>
+                    <div>
+
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
+    )
+}
+export default Card;
